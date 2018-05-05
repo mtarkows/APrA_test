3a9475288e3ee3f04679047bb9891e24dabe69e5
mtarkows <36234672+mtarkows@users.noreply.github.com>
2018-05-05 17:18:25 -0300

    Add files via upload

---
+++ puntos_verdes_test1/data/PuntosVerdesconpesajeAED2017_1.js
@@ -0,0 +1,10 @@
+var json_PuntosVerdesconpesajeAED2017_1 = {
+"type": "FeatureCollection",
+"name": "PuntosVerdesconpesajeAED2017_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "14", "plaza": "Punto Verde Especial - Parque Centenario", "tipo": "Punto Verde Especial", "cooperativ": null, "direccion_": "PATRICIAS ARGENTINAS AV. y ROENTGEN, AV.", "barrio": "Caballito", "AED2017_AT": "12.104,00", "q2wHide_pv_pesaje_all_ano_aed_2017_int": 12104.0 }, "geometry": { "type": "Point", "coordinates": [ -58.433050614713515, -34.60644586669801 ] } },
+{ "type": "Feature", "properties": { "id": "25", "plaza": "Punto Verde Especial - Plaza Arenales", "tipo": "Punto Verde Especial", "cooperativ": null, "direccion_": "NUEVA YORK y MERCEDES", "barrio": "Villa Devoto", "AED2017_AT": "15.525,00", "q2wHide_pv_pesaje_all_ano_aed_2017_int": 15525.0 }, "geometry": { "type": "Point", "coordinates": [ -58.512429257173757, -34.600354193198534 ] } },
+{ "type": "Feature", "properties": { "id": "27", "plaza": "Parque Avellaneda", "tipo": "Punto Verde Con Atención", "cooperativ": "RUO", "direccion_": "DIRECTORIO AV. y FERNANDEZ", "barrio": "Parque Avellaneda", "AED2017_AT": "287,00", "q2wHide_pv_pesaje_all_ano_aed_2017_int": 287.0 }, "geometry": { "type": "Point", "coordinates": [ -58.481794724341938, -34.643331945208764 ] } }
+]
+}
