3b95b9bb142d6160ea8b7bde6eaf7de9242ef83d
mtarkows <36234672+mtarkows@users.noreply.github.com>
2018-05-05 15:25:01 -0300

    Add files via upload

---
+++ ruido_test1/data/RuidoPromediodemedicioneshorariasderuidodBA_0.js
@@ -0,0 +1,23 @@
+var json_RuidoPromediodemedicioneshorariasderuidodBA_0 = {
+"type": "FeatureCollection",
+"name": "RuidoPromediodemedicioneshorariasderuidodBA_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "TMI": "1286", "BARRIO": "NUEVA POMPEYA", "DIRECCION": "QUILMES 436", "cs_2011_2013_CS_2013": 59.96 }, "geometry": { "type": "Point", "coordinates": [ -58.417315889361149, -34.644610581321764 ] } },
+{ "type": "Feature", "properties": { "TMI": "1318", "BARRIO": "SAN NICOLAS", "DIRECCION": "VIAMONTE 808", "cs_2011_2013_CS_2013": 70.75 }, "geometry": { "type": "Point", "coordinates": [ -58.378385879345032, -34.600114281342663 ] } },
+{ "type": "Feature", "properties": { "TMI": "1314", "BARRIO": "RECOLETA", "DIRECCION": "AV. SANTA FE 1748", "cs_2011_2013_CS_2013": 74.4 }, "geometry": { "type": "Point", "coordinates": [ -58.392583839349705, -34.595927851337507 ] } },
+{ "type": "Feature", "properties": { "TMI": "1291", "BARRIO": "PARQUE PATRICIOS", "DIRECCION": "AV. CASEROS 2526", "cs_2011_2013_CS_2013": 69.13 }, "geometry": { "type": "Point", "coordinates": [ -58.397795819353739, -34.635773371330806 ] } },
+{ "type": "Feature", "properties": { "TMI": "1304", "BARRIO": "BALVANERA", "DIRECCION": "CORRIENTES 2937", "cs_2011_2013_CS_2013": 73.63 }, "geometry": { "type": "Point", "coordinates": [ -58.407087409355277, -34.604263151330684 ] } },
+{ "type": "Feature", "properties": { "TMI": "1305", "BARRIO": "ALMAGRO", "DIRECCION": "AV. RIVADAVIA 3883", "cs_2011_2013_CS_2013": 74.96 }, "geometry": { "type": "Point", "coordinates": [ -58.42077617936058, -34.611310581324247 ] } },
+{ "type": "Feature", "properties": { "TMI": "1315", "BARRIO": "CABALLITO", "DIRECCION": "AV. JOSE MARAA MORENO 121", "cs_2011_2013_CS_2013": 74.43 }, "geometry": { "type": "Point", "coordinates": [ -58.435840429366529, -34.619722171316937 ] } },
+{ "type": "Feature", "properties": { "TMI": "1316", "BARRIO": "FLORES", "DIRECCION": "AV. SAN PEDRITO 60", "cs_2011_2013_CS_2013": 72.46 }, "geometry": { "type": "Point", "coordinates": [ -58.469169309379652, -34.631496131301084 ] } },
+{ "type": "Feature", "properties": { "TMI": "1292", "BARRIO": "PARQUE AVELLANEDA", "DIRECCION": "REMEDIOS 3761", "cs_2011_2013_CS_2013": 59.25 }, "geometry": { "type": "Point", "coordinates": [ -58.476655389383026, -34.641397481296593 ] } },
+{ "type": "Feature", "properties": { "TMI": "1282", "BARRIO": "MATADEROS", "DIRECCION": "AV. EVA PERON 5251", "cs_2011_2013_CS_2013": 61.92 }, "geometry": { "type": "Point", "coordinates": [ -58.481991199386094, -34.659832631291977 ] } },
+{ "type": "Feature", "properties": { "TMI": "1284", "BARRIO": "VELEZ SARSFIELD", "DIRECCION": "AV. SEGUROLA 726", "cs_2011_2013_CS_2013": 70.96 }, "geometry": { "type": "Point", "coordinates": [ -58.489602549387378, -34.628255931292301 ] } },
+{ "type": "Feature", "properties": { "TMI": "1313", "BARRIO": "PALERMO", "DIRECCION": "AV. SANTA FE 3259", "cs_2011_2013_CS_2013": 73.54 }, "geometry": { "type": "Point", "coordinates": [ -58.411047319355831, -34.588294391330926 ] } },
+{ "type": "Feature", "properties": { "TMI": "1289", "BARRIO": "BELGRANO", "DIRECCION": "AV. JURAMENTO 2427", "cs_2011_2013_CS_2013": 71.25 }, "geometry": { "type": "Point", "coordinates": [ -58.457095859371336, -34.5622267813146 ] } },
+{ "type": "Feature", "properties": { "TMI": "1295", "BARRIO": "VILLA URQUIZA", "DIRECCION": "CONGRESO 4364", "cs_2011_2013_CS_2013": 69.0 }, "geometry": { "type": "Point", "coordinates": [ -58.48233354938121, -34.565741591303059 ] } },
+{ "type": "Feature", "properties": { "TMI": "1296", "BARRIO": "VILLA DEVOTO", "DIRECCION": "AV. FRANCISCO BEIRO 3994", "cs_2011_2013_CS_2013": 71.29 }, "geometry": { "type": "Point", "coordinates": [ -58.508098379393367, -34.603637681286777 ] } },
+{ "type": "Feature", "properties": { "TMI": "1283", "BARRIO": "LINIERS", "DIRECCION": "LISANDRO DE LA TORRE 39", "cs_2011_2013_CS_2013": 71.67 }, "geometry": { "type": "Point", "coordinates": [ -58.521273409400521, -34.639380651276262 ] } }
+]
+}
