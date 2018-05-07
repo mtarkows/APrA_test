36d0c5821d0fb5043a16aee78d2a3af6cd0fab32
mtarkows <36234672+mtarkows@users.noreply.github.com>
2018-05-07 15:03:29 -0300

    Add files via upload

---
+++ salud_social_test1/data/Centrointegralesdelamujer_6.js
@@ -0,0 +1,18 @@
+var json_Centrointegralesdelamujer_6 = {
+"type": "FeatureCollection",
+"name": "Centrointegralesdelamujer_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "MARIA GALLEGO", "DIRECCION": "BEIRO, FRANCISCO AV. 5229", "BARRIO": "VILLA DEVOTO", "COMUNA_NO": 11.0 }, "geometry": { "type": "Point", "coordinates": [ -58.524155309159802, -34.613317372615008 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "MINERVA MIRABAL", "DIRECCION": "FERNANDEZ DE LA CRUZ, F., GRAL. AV. 4208", "BARRIO": "VILLA SOLDATI", "COMUNA_NO": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -58.456996349561194, -34.671459073805259 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ALICIA MOREAU", "DIRECCION": "URIBURU JOSE E., PRES. 1022", "BARRIO": "RECOLETA", "COMUNA_NO": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -58.398845724567423, -34.596663906479257 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ISABEL CALVO", "DIRECCION": "PIEDRAS 1281", "BARRIO": "CONSTITUCION", "COMUNA_NO": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -58.377167538004109, -34.623175292721321 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ELVIRA RAWSON", "DIRECCION": "SALGUERO, JERONIMO 765", "BARRIO": "ALMAGRO", "COMUNA_NO": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -58.419186366658188, -34.601251949685846 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "MARGARITA MALHARRO", "DIRECCION": "24 DE NOVIEMBRE 113", "BARRIO": "BALVANERA", "COMUNA_NO": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -58.411685935706693, -34.612089047040996 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "LUGAR DE MUJER", "DIRECCION": "FRAGATA PRES. SARMIENTO 889", "BARRIO": "CABALLITO", "COMUNA_NO": 6.0 }, "geometry": { "type": "Point", "coordinates": [ -58.453284839253385, -34.6138965732519 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "DIGNXS DE SER", "DIRECCION": "LAUTARO 188", "BARRIO": "FLORES", "COMUNA_NO": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -58.456589891234387, -34.629441354682598 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "TRAYECTORIA POR UNA VIDA SIN VIOLENCIA", "DIRECCION": "ZAPIOLA 4583", "BARRIO": "SAAVEDRA", "COMUNA_NO": 12.0 }, "geometry": { "type": "Point", "coordinates": [ -58.478951443692885, -34.54442574002293 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "PEPA GAITAN", "DIRECCION": "PICHINCHA AV. 1765", "BARRIO": "PARQUE PATRICIOS", "COMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.396436800152188, -34.629996529250896 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ARMINDA ABERASTURY", "DIRECCION": "YRIGOYEN, HIPOLITO 3202", "BARRIO": "BALVANERA", "COMUNA_NO": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -58.411871754107501, -34.61198829480734 ] } }
+]
+}
---
+++ salud_social_test1/data/Centrosdedesarrolloinfantil_5.js
@@ -0,0 +1,27 @@
+var json_Centrosdedesarrolloinfantil_5 = {
+"type": "FeatureCollection",
+"name": "Centrosdedesarrolloinfantil_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "AGRONOMIA", "DIRECCION": "AVENIDA DE LOS CONSTITUYENTES 3110", "BARRIO": "AGRONOMIA", "COAMUNA_NO": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.478035789014456, -34.591132776686642 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ALBARELLOS", "DIRECCION": "AVENIDA ALBARELLOS 3153", "BARRIO": "VILLA PUEYRREDON", "COAMUNA_NO": 12.0 }, "geometry": { "type": "Point", "coordinates": [ -58.512482232644921, -34.579053699076994 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ANDRES FERREYRA", "DIRECCION": "AVENIDA DIAZ VELEZ 4633", "BARRIO": "CABALLITO", "COAMUNA_NO": 6.0 }, "geometry": { "type": "Point", "coordinates": [ -58.432502500553966, -34.608657389515251 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "BAM-BAM", "DIRECCION": "VENTURA ALEGRE 799", "BARRIO": null, "COAMUNA_NO": null }, "geometry": { "type": "Point", "coordinates": [ -58.454150049255965, -34.60286551472641 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "BARRIO COPELLO", "DIRECCION": "AVENIDA DELLEPIANE 4900 (NORTE)", "BARRIO": "VILLA LUGANO", "COAMUNA_NO": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -58.473531418985807, -34.666540610252667 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CHACABUCO", "DIRECCION": "AVENIDA EVA PERON 1502", "BARRIO": "PARQUE CHACABUCO", "COAMUNA_NO": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -58.444896308802626, -34.633102757100261 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CRECIENDO", "DIRECCION": "AVENIDA ALMANCIO ALCORTA 1402", "BARRIO": "BARRACAS", "COAMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.383706932739315, -34.633353063031926 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "DIENTITOS DE LECHE", "DIRECCION": "VIAMONTE 872", "BARRIO": "SAN NICOLAS", "COAMUNA_NO": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -58.379277413234313, -34.600150074303691 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ESPORA", "DIRECCION": "OSCAR BONAVENA Y SANTO DOMINGO", "BARRIO": "NUEVA POMPEYA", "COAMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.404758944836864, -34.65490807203804 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "FANTASSIA Y GARABATOS", "DIRECCION": "VIRREY CEVALLOS 1333", "BARRIO": "CONSTITUCION", "COAMUNA_NO": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -58.388495187292904, -34.624120843436437 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "HERRERA", "DIRECCION": "HERRERA 566", "BARRIO": "BARRACAS", "COAMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.37821468259213, -34.636176060199148 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "MI MUNDO FELIZ", "DIRECCION": "AVENIDA WARNES 2630", "BARRIO": "PATERNAL", "COAMUNA_NO": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.4758887574529, -34.59712131564303 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "PATRICIOS", "DIRECCION": "AVENIDA ALMAFUERTE 66", "BARRIO": "PARQUE PATRICIOS", "COAMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.411610504981496, -34.639055963261669 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "PIEDRABUENA", "DIRECCION": "CRISOSTOMO ALVARAZ 6750", "BARRIO": "VILLA LUGANO", "COAMUNA_NO": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -58.492659098569234, -34.677646265486679 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "PRINGLES", "DIRECCION": "PRINGLES 344", "BARRIO": "ALMAGRO", "COAMUNA_NO": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -58.427763057875573, -34.608012033871169 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "QUINQUELA MARTIN", "DIRECCION": "AVENIDA DON PEDRO DE MENDOZA 1801", "BARRIO": "BOCA", "COAMUNA_NO": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -58.360555912796102, -34.638967843933372 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "RAYITO DE SOL", "DIRECCION": "AVENIDA ESCALADA 4222", "BARRIO": "VILLA SOLDATI", "COAMUNA_NO": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -58.458598212128578, -34.673162515419094 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "SAAVEDRA", "DIRECCION": "PINTO 3910", "BARRIO": "SAAVEDRA", "COAMUNA_NO": 12.0 }, "geometry": { "type": "Point", "coordinates": [ -58.477712443315433, -34.549445018649173 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "TUTZO DE BONIFACIO", "DIRECCION": "VICTOR HUGO 682", "BARRIO": "VILLA LURO", "COAMUNA_NO": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.506882383208229, -34.632511256494368 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "VERA PEÑALOZA", "DIRECCION": "GIRIBONE 976", "BARRIO": "CHACARITA", "COAMUNA_NO": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.456215031415724, -34.580251248994266 ] } }
+]
+}
